b0449a07ac2aeb8489350d911fe3d390ba77ae11
Timon345full <itcodingpythontoday@gmail.com>
2025-04-04 18:18:15 +0300

    Audio mdifucate control

--- tools/ControlExernDevice/audioSound.py
+++ tools/ControlExernDevice/audioSound.py
@@ -2,6 +2,8 @@ import pyaudio
 import speech_recognition as sr # для считывания записи с микрафона и переобразования в текст 
 import tkinter as tk
 import keyboard # для увелечения значения в функции readMicrophone в переменной record_second чтоб когда нажимал на кнопку и он добовлял значение переменой на 1 минуту и больше 
+import wave
+
 
 def readMicrophone(DeviceIndex=1, timeout=7, phrase_time_limit=5):
     windows = tk.Tk()
@@ -56,6 +58,51 @@ def readMicrophone(DeviceIndex=1, timeout=7, phrase_time_limit=5):
     txt_label.place(x=100, y=408)
     # button_save_audio.place(x=30, y=10)
     windows.mainloop()
-
+    
 def readMicroAndSave(filename="recorded.wav",  record_second=5):
-    pass
+    # имя файла для записи
+    filename = "recorded.mp3"
+    # установить размер блока в 1024 сэмпла
+    chunk = 1024
+    # образец формата
+    FORMAT = pyaudio.paInt16
+    # моно, если хотите стере измените на 2
+    channels = 1
+    # 44100 сэмплов в секунду
+    sample_rate = 44100
+    record_seconds = 5
+    # initialize PyAudio object
+    p = pyaudio.PyAudio()
+    # открыть объект потока как ввод и вывод
+    stream = p.open(format=FORMAT,
+                    channels=channels,
+                    rate=sample_rate,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+    frames = []
+    print("Recording...")
+    for i in range(int(44100 / chunk * record_seconds)):
+        data = stream.read(chunk)
+        # если вы хотите слышать свой голос во время записи
+        # stream.write(data)
+        frames.append(data)
+    print("Finished recording.")
+    # остановить и закрыть поток
+    stream.stop_stream()
+    stream.close()
+    # завершить работу объекта pyaudio
+    p.terminate()
+    # сохранить аудиофайл
+    # открываем файл в режиме 'запись байтов'
+    wf = wave.open(filename, "wb")
+    # установить каналы
+    wf.setnchannels(channels)
+    # установить формат образца
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    # установить частоту дискретизации
+    wf.setframerate(sample_rate)
+    # записываем кадры как байты
+    wf.writeframes(b"".join(frames))
+    # закрыть файл
+    wf.close()
